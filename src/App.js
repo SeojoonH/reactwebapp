231960fce0986b45028866052ddbba08c8783428
seojoon <iamaloneinseoul@gmail.com>
2023-07-14 11:55:18 +0900

    third commit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+
 import Navbar from "./components/navbar/Navbar";
 import Hero from "./components/hero/Hero";
 import Destinations from "./components/destinations/Destinations";
